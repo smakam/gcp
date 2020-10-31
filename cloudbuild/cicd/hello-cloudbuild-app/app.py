3f1133a56610c5d9bfeaae254fb1df1c31d5bcba
Sreenivas Makam <sreemakam@google.com>
2020-10-31 20:27:04 +0530

    changed hello

--- cloudbuild/cicd/hello-cloudbuild-app/app.py
+++ cloudbuild/cicd/hello-cloudbuild-app/app.py
@@ -18,7 +18,7 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "Hello Cloud Build!\n"
+  return "Hello Cloud!\n"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
